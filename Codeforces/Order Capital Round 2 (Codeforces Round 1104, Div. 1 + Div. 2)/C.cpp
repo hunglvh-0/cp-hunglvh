f2695804f97b78c312de77a119a9a93ecde8f7cf
hunglvh-0 <hunglvh36@gmail.com>
2026-06-19 06:04:34 +0700

    updated

---
+++ Codeforces/Order Capital Round 2 (Codeforces Round 1104, Div. 1 + Div. 2)/C.cpp
@@ -0,0 +1,92 @@
+/**
+ * Author:  hunglvh
+ * Created: 2026-06-18 21:51:47
+ * ❤️C
+**/
+#include<bits/stdc++.h>
+//hunglvh - The king is back
+
+using namespace std;
+#define ll long long
+#define ld long double
+#define all(x) x.begin(), x.end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define el '\n'
+#define arr(a,n) for (int i = 0; i < n; i++) cin >> a[i];
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define yes cout<<"YES"
+#define no cout<<"NO"
+#define time (clock()*1.0/CLOCKS_PER_SEC)
+using u64 = uint64_t;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+#define f first
+#define s second
+#define i128 __int128
+ll gcd(ll a, ll b) { return std::gcd(a, b); }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+ll pow_mod(ll a, ll b, ll m = MOD) {
+    ll res = 1; a %= m;
+    while (b > 0) { if (b & 1) res = (res * a) % m; a = (a * a) % m; b >>= 1; }
+    return res;
+}
+#ifndef ONLINE_JUDGE
+    #define DEBUG_TIME(t_) cerr << "Case #" << t_ << ": " << time << "s" << el;
+#else
+    #define DEBUG_TIME(t_)
+#endif
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << el;
+#else
+#define debug(x)
+#endif
+inline void solve() {
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        vector<ll> a(n);
+        for(int i=0;i<n;i++)cin>>a[i];
+        ll l=0,r=200000000000000,ans=r;
+        while(l<=r){
+            ll m=l+(r-l)/2;
+            ll b=m;
+            bool ok=true;
+            for(int i=n-1;i>=0;i--){
+                if(a[i]<=b){
+                    b=max(a[i],b-a[i]);
+                }
+                else{
+                    b-=a[i];
+                }
+                if(b<0){
+                    ok=false;break;
+                }
+            }
+            if(ok){ans=m;r=m-1;}
+            else {
+                l=m+1;
+            }
+        }
+        cout<<ans<<el;
+    }
+}
+int32_t main() {
+    fast;
+    #define file "main"
+    if(fopen(file".INP","r")) {
+        freopen(file".INP","r",stdin);
+        freopen(file".OUT","w",stdout);
+    }
+    solve();
+    return 0;
+}
