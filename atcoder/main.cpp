ffde3df40d46236c9c7ea86c7c72f9fd6202cc62
hunglvh-0 <hunglvh36@gmail.com>
2026-06-22 20:21:25 +0700

    updated

---
+++ atcoder/main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define el "\n"
+#define file ""
+inline void sol(){
+
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    if(fopen(file".INP","r")) {
+        freopen(file".INP","r",stdin);
+        freopen(file".OUT","w",stdout);
+    }
+}
+
+
+
+
