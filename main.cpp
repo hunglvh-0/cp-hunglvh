a6109c8fa7cbe1d25bd6e4532dd8e00d5bd4251a
hunglvh-0 <hunglvh36@gmail.com>
2026-06-23 20:30:21 +0700

    updated

---
+++ main.cpp
@@ -0,0 +1,78 @@
+/**
+ * Author:  hunglvh
+ * Created: 2026-06-23 20:07:33
+ * ❤️C
+**/
+#include<bits/stdc++.h>
+//hunglvh - The king is back
+
+using namespace std;
+#define ll long long
+#define ld long double
+#define all(x) x.begin(), x.end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define el '\n'
+#define arr(a,n) for (int i = 0; i < n; i++) cin >> a[i];
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define yes cout<<"YES"
+#define no cout<<"NO"
+#define time (clock()*1.0/CLOCKS_PER_SEC)
+using u64 = uint64_t;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vs=vector<string>;
+using vc=vector<char>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+#define f first
+#define s second
+#define i128 __int128
+ll gcd(ll a, ll b) { return std::gcd(a, b); }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+ll pow_mod(ll a, ll b, ll m = MOD) {
+    ll res = 1; a %= m;
+    while (b > 0) { if (b & 1) res = (res * a) % m; a = (a * a) % m; b >>= 1; }
+    return res;
+}
+#ifndef ONLINE_JUDGE
+    #define DEBUG_TIME(t_) cerr << "Case #" << t_ << ": " << time << "s" << el;
+#else
+    #define DEBUG_TIME(t_)
+#endif
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << el;
+#else
+#define debug(x)
+#endif
+inline void solve() {
+    int t;cin>>t;
+    while(t--){
+        int n,k;cin>>n>>k;
+        string s;cin>>s;
+        map<int,int>c;
+        for(int i=0;i<n;i++){
+                if(s[i]=='1')c[i%k]++;
+        }
+        string ans="YES";
+        for(int i=0;i<k;i++){
+                if(c[i]%2!=0)ans="NO";
+            }
+            cout<<ans<<el;
+	}
+}
+int32_t main() {
+    fast;
+    #define file "main"
+    if(fopen(file".INP","r")) {
+        freopen(file".INP","r",stdin);
+        freopen(file".OUT","w",stdout);
+    }
+    solve();
+    return 0;
+}
