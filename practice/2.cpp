92c39a18c3e5bba85c94b045ca445f186968778e
hunglvh-0 <hunglvh36@gmail.com>
2026-06-25 15:16:39 +0700

    updated

---
+++ practice/2.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define el "\n"
+#define task ""
+inline void sol(){
+    cout<<"3 x 1 = 3"<<el;
+    cout<<"3 x 2 = 6"<<el;
+    cout<<"3 x 3 = 9"<<el;
+    cout<<"3 x 4 = 12"<<el;
+    cout<<"3 x 5 = 15"<<el;
+    cout<<"3 x 6 = 18"<<el;
+    cout<<"3 x 7 = 21"<<el;
+    cout<<"3 x 8 = 24"<<el;
+    cout<<"3 x 9 = 27"<<el;
+    cout<<"3 x 10 = 30"<<el;
+    cout<<el;
+    cout<<"6 x 1 = 6"<<el;
+    cout<<"6 x 2 = 12"<<el;
+    cout<<"6 x 3 = 18"<<el;
+    cout<<"6 x 4 = 24"<<el;
+    cout<<"6 x 5 = 30"<<el;
+    cout<<"6 x 6 = 36"<<el;
+    cout<<"6 x 7 = 42"<<el;
+    cout<<"6 x 8 = 48"<<el;
+    cout<<"6 x 9 = 54"<<el;
+    cout<<"6 x 10 = 60"<<el;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    if(fopen(task".INP","r")){
+        freopen(task".INP","r",stdin);
+        freopen(task".OUT","w",stdout);
+    }
+    sol();
+}
--- practice/3.cpp
+++ practice/3.cpp
@@ -4,13 +4,14 @@ using namespace std;
 #define el "\n"
 #define task ""
 inline void sol(){
-    cout << "C754324 - Dao Thanh Thuy - VISA - 612415"<<el;
-    cout << "C754382 - Ngo Binh Phu - VISA - 127524"<<el;
-    cout << "A769245 - Duong Manh Quan - VISA - 12489621"<<el;
+    cout<<"*"<<el;
+    cout<<"**"<<el;
+    cout<<"***"<<el;
+    cout<<"****"<<el;
+    cout<<"*****"<<el;
 }
 
-int main()
-{
+int main(){
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     if(fopen(task".INP","r")){
         freopen(task".INP","r",stdin);
